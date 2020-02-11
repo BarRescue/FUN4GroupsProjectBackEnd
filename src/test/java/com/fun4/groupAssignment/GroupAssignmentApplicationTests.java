a9bde16d66eb18d1150925e275de0cafdb11394d
Odin van der Linden <Odinvanderlinden2001@gmail.com>
2020-02-11 13:53:21 +0100

    spring setup

---
+++ src/test/java/com/fun4/groupAssignment/GroupAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fun4.groupAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
