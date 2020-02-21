3be08501a1b12cfd5792a503ba22f2bef3018f56
Rens Manders <info@rensmanders.nl>
2020-02-22 00:24:20 +0100

    Get current user

--- src/main/java/App/controllers/auth/LoginController.java
+++ src/main/java/App/controllers/auth/LoginController.java
@@ -10,10 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
@@ -57,5 +56,12 @@ public class LoginController {
         } catch(AuthenticationException ex) {
             return new ResponseEntity<>(AuthResponse.UNEXPECTED_ERROR.toString(), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity currentUser(@AuthenticationPrincipal UserDetails userDetails) {
+        Map<Object, Object> model = new LinkedHashMap<>();
+        model.put("user", userDetails);
+        return ok(model);
     }
 }
